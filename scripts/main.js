d23013962c620a37d99b96eae6d9ba1a6f6dbee8
Kairat <kairatu@mail.ru>
2025-05-23 10:18:31 +0500

    Commit with  jules

--- scripts/main.js
+++ scripts/main.js
@@ -8,20 +8,6 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/bax.jpg');
     }
 }
-/*
-Всё, что находится тут - комментарий.
-*/
-// Это комментарий
-/*
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
-*/
-/*
-эквивалентно
-var myHTML = document.querySelector('html');
-myHTML.onclick = function() {};
-*/
 
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
@@ -29,18 +15,14 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML  = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML  = 'Mozilla - это круто, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.innerHTML  = 'Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick = function() {
-  setUserName();
+  myHeading.innerHTML  = 'Mozilla - это круто, ' + storedName;
 }
 
 
